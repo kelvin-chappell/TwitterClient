c4a4cbd141d069800595200a37dd04861f77db06
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2012-06-28 12:54:35 +0100

    Moved twitter properties.

--- src/main/scala/twitterclient/Client.scala
+++ src/main/scala/twitterclient/Client.scala
@@ -10,15 +10,10 @@ import twitter4j.TwitterFactory
  *
  */
 object Client {
+
   def main(args: Array[String]) {
 
-    val cb = new ConfigurationBuilder()
-    cb.setDebugEnabled(true)
-      .setOAuthConsumerKey("2L19WvwxwQspIYdZyjYxQ")
-      .setOAuthConsumerSecret("tVkClERxjPLecfxu4tPECcjQnrIIMPTAJotWTCp7xU")
-      .setOAuthAccessToken("620893056-EZsrKROHgv9eAuIW5Ax3Pcqt5wuisc7i1Qr2x3s")
-      .setOAuthAccessTokenSecret("wXK7eybV3bNZlLkNS6kjwDnd2HG0As2gUC12y7oFM")
-    val tf = new TwitterFactory(cb.build())
+    val tf = new TwitterFactory()
     val twitter = tf.getInstance()
 
     val statuses = twitter.getHomeTimeline
