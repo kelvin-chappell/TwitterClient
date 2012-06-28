ec4a10178f3db68ee824f4116d95603662810b7a
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2012-06-28 13:23:41 +0100

    Refactored.

--- src/main/scala/twitterclient/Client.scala
+++ src/main/scala/twitterclient/Client.scala
@@ -12,19 +12,13 @@ import twitter4j.TwitterFactory
 object Client {
 
   def main(args: Array[String]) {
-
-    val tf = new TwitterFactory()
-    val twitter = tf.getInstance()
-
+    val twitter = new TwitterFactory().getInstance
     val statuses = twitter.getHomeTimeline
-    println("Showing timeline")
-    val it = statuses.iterator()
-    while (it.hasNext()) {
-      val status = it.next()
-      println(status.getUser().getName() + ":" +
-        status.getText());
+    val it = statuses.iterator
+    while (it.hasNext) {
+      val status = it.next
+      println(status.getUser.getName + ": " + status.getText)
     }
-
   }
 
 }
